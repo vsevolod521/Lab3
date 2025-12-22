2a5a775928935b14d88c918f35651a6637cfa69e
Vsevolod <VKrupenin55@mail.ru>
2025-12-22 20:09:44 +0300

    сортировка + бенчмарки

--- sorting.c
+++ sorting.c
@@ -105,3 +105,72 @@ static void split_stack(void *p_container, void *left, void *right)
 
     free_stack(temp);
 }
+void merge_sort(void *p_container)
+{
+    if (p_container == NULL)
+        return;
+
+    int size = get_size_stack(p_container);
+    if (size <= 1)
+        return;
+
+    void *left = create_stack();
+    void *right = create_stack();
+
+    split_stack(p_container, left, right);
+    merge_sort(left); 
+    merge_sort(right); 
+
+    void *merged = merge_two_stacks(left, right);
+
+    while (!is_empty_stack(merged))
+    {
+        push_back_stack(p_container, top_stack(merged));
+        pop_back_stack(merged);
+    }
+    free_stack(merged);
+}
+
+void benchmark_sorts(int max_size, int step)
+{
+    srand(12345); 
+
+    printf("Размер данных;Insertion Sort (мс);Merge Sort (мс)\n");
+
+    for (int size = step; size <= max_size; size += step)
+    {
+        int *test_data = (int *)malloc(size * sizeof(int));
+        for (int i = 0; i < size; i++)
+        {
+            test_data[i] = rand() % 10000;
+        }
+
+        void *stack1 = create_stack();
+        for (int i = 0; i < size; i++)
+        {
+            push_back_stack(stack1, test_data[i]);
+        }
+
+        clock_t start = clock();
+        insertion_sort(stack1); 
+        clock_t end = clock();
+        double time_insertion = ((double)(end - start) * 1000) / CLOCKS_PER_SEC;
+        free_stack(stack1);
+
+        void *stack2 = create_stack();
+        for (int i = 0; i < size; i++)
+        {
+            push_back_stack(stack2, test_data[i]);
+        }
+
+        start = clock();
+        merge_sort(stack2);
+        end = clock();
+        double time_merge = ((double)(end - start) * 1000) / CLOCKS_PER_SEC;
+        free_stack(stack2);
+
+        printf("%d;%.3f;%.3f\n", size, time_insertion, time_merge);
+
+        free(test_data);
+    }
+}
--- sorting.h
+++ sorting.h
@@ -1,3 +1,5 @@
 #pragma once
 
 void insertion_sort(void *); 
+void merge_sort(void *);
+void benchmark_sorts(int, int);
